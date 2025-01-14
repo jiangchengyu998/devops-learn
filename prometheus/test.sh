ceefeab61430f61dd9480bdf1d2574c8564df532
hanke <jchengyu0829@163.com>
2025-01-14 21:49:55 +0800

    Update test.sh

--- prometheus/test.sh
+++ prometheus/test.sh
@@ -2,9 +2,9 @@
 
 for (( i=1;i<10000;i++ ));
 do
-  curl http://192.168.101.102:8090/ping
+  # curl http://192.168.101.102:8090/ping
   # curl http://192.168.101.102:8082/hello
-  # curl http://192.168.101.102:8083/hello
+  curl http://192.168.101.102:8083/hello
   echo ""
   echo "sleep 0.5s"
   sleep 0.1
